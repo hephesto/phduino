7f2fc868e35347fec1af9996333bc77bb7c077a6
Carlos A. Neves <hephesto@users.noreply.github.com>
2015-04-03 17:01:32 -0300

    Create freemem.h

---
+++ firmware/command_line/freemem.h
@@ -0,0 +1,14 @@
+// this function will return the number of bytes currently free in RAM
+// written by David A. Mellis
+// based on code by Rob Faludi http://www.faludi.com
+int availableMemory() {
+  int size = 1024; // Use 2048 with ATmega328
+  byte *buf;
+
+  while ((buf = (byte *) malloc(--size)) == NULL)
+    ;
+
+  free(buf);
+
+  return size;
+}
